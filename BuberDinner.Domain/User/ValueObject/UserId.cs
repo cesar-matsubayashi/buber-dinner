755dd0a0f487f9e582017d4a76d4e3bac5fa1f83
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-05-06 16:36:54 -0300

    feat: add userid create method

--- BuberDinner.Domain/User/ValueObject/UserId.cs
+++ BuberDinner.Domain/User/ValueObject/UserId.cs
@@ -16,6 +16,11 @@ namespace BuberDinner.Domain.User.ValueObjects
             return new(Guid.NewGuid());
         }
 
+        public static UserId Create(Guid value)
+        {
+            return new(value);
+        }
+
         public override IEnumerable<object> GetEqualityComponents()
         {
             yield return Value;
