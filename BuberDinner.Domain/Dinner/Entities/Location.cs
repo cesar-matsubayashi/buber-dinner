e006768b661a43118f1faa0fec948924b89aea6b
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-05-08 11:28:51 -0300

    feat: add identity to location

--- BuberDinner.Domain/Dinner/Entities/Location.cs
+++ BuberDinner.Domain/Dinner/Entities/Location.cs
@@ -1,16 +1,54 @@
-﻿namespace BuberDinner.Domain.Dinner.Entities
-{
-    public sealed class Location
-    {
-        public string Name { get; } 
-        public string Address { get; } 
-        public float Latitude { get; } 
-        public float Longitude { get; }
+﻿using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Dinner.ValueObjects;
+using BuberDinner.Domain.Host.ValueObjects;
 
-        public Location(
+namespace BuberDinner.Domain.Dinner.Entities
+{
+    public sealed class Location : AggregateRoot<LocationId>
+    {
+        public string Name { get; private set; } 
+        public string Address { get; private set;} 
+        public float Latitude { get; private set;} 
+        public float Longitude { get; private set;}
+        public HostId HostId { get; private set;}
+
+        private Location(
+            LocationId locationId,
             string name, 
             string address, 
             float latitude, 
+            float longitude,
+            HostId hostId)
+            : base(locationId)
+        {
+            
+            Name = name;
+            Address = address;
+            Latitude = latitude;
+            Longitude = longitude;
+            HostId = hostId;
+        }
+
+        public static Location Create(
+            string name,
+            string address,
+            float latitude,
+            float longitude,
+            HostId hostId)
+        {  
+            return new(
+                LocationId.CreateUnique(),
+                name, 
+                address, 
+                latitude, 
+                longitude, 
+                hostId);
+        }
+
+        public void Update (
+            string name,
+            string address,
+            float latitude,
             float longitude)
         {
             Name = name;
@@ -18,16 +56,10 @@
             Latitude = latitude;
             Longitude = longitude;
         }
-        public static Location Create(
-            string name,
-            string address,
-            float latitude,
-            float longitude)
-        {  
-            return new(name, address, latitude, longitude);
-        }
 
-
+#pragma warning disable CS8618
+        private Location() { }
+#pragma warning restore CS8618 
 
     }
 }
